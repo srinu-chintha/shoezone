519dcc51d0b0ffeb903383d374a191878e45b711
srinuchintha <chinthasrinu95745@gmail.com>
2021-09-30 18:53:43 +0530

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
@@ -92,7 +92,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'thawing-everglades-25233.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'https://thawing-everglades-25233.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
@@ -116,6 +116,5 @@ Rails.application.configure do
     config.action_mailer.perform_deliveries = true
 
     config.log_level = :debug
-    #did on change above
 
 end

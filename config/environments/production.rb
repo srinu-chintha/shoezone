8ac40afe7f7d3b64f995f2a2a6c302e148b16522
srinuchintha <chinthasrinu95745@gmail.com>
2021-09-30 17:00:54 +0530

    added http

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
 
 
 
-  config.action_mailer.default_url_options = { host: 'https://thawing-everglades-25233.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'thawing-everglades-25233.herokuapp.com',:protocol => 'http'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
